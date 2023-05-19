abc9c891072ba66a0b99805d655da65ea209422d
Faiyaz Rafeek <fayaznahwi@gmail.com>
2023-05-19 11:35:30 +0530

    initial REST API for goals model

--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,10 +1,13 @@
 const asyncHnadler = require('express-async-handler')
 
+const Goal = require('../model/goalModel')
+
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHnadler(async (req, res) => {
-    res.status(200).json({message : "Get Goals"})
+    const goals = await Goal.find()
+    res.status(200).json(goals)
 })
 
 // @desc    Set goal
@@ -16,21 +19,41 @@ const setGoal = asyncHnadler(async (req, res) => {
         throw new Error('Please add a text field ')
     }
 
-    res.status(200).json({message : "Set Goal"})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
 
 // @desc    Update goal
 // @route   PUT /api/goals/:id
 // @access  Private
 const updateGoal = asyncHnadler(async (req, res) => {
-    res.status(200).json({message : `Update Goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.status(200).json(updatedGoal)
 })
 
 // @desc    Detele goal
 // @route   DELETE /api/goals/:id
 // @access  Private
 const deleteGoal = asyncHnadler(async (req, res) => {
-    res.status(200).json({message : `deleted Goal ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    await goal.remove
+
+    res.status(200).json({id : req.params.id})
 })
 
 
